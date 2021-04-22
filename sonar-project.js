5ea069b9d5573b7fa20ef0ace566b0883d5f818a
karthik19-cloud <32713545+karthik19-cloud@users.noreply.github.com>
2021-04-22 20:34:27 +0530

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,15 @@
+const sonarqubeScanner = require("sonarqube-scanner");
+sonarqubeScanner(
+  {
+    serverUrl: "https://sonarqube.ct.blue.cdtapps.com",
+    token: "ada045feefa7978777074d1326b8ade8aee9fa7c",
+    options: {
+     "sonar.projectKey": "cweb.cweb-pilot-test",
+      "sonar.projectName": "cweb-pilot-test",
+      "sonar.sources": "./src",
+      "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
+      "sonar.testExecutionReportPaths": "reports/test-report.xml",
+    },
+  },
+  () => {},
+  );
